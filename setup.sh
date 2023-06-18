7694013e861f9c5b323821c66a4505d455e40302
Alexander <orachigami@mail.ru>
2023-06-19 02:23:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-pkg remove -y game-repo &&
+export DEBIAN_FRONTEND=noninteractive &&
+  echo -e 'Dpkg::Options {\n  "--force-confnew";\n}' > ~/../usr/etc/apt/apt.conf.d/local &&
+  pkg remove -y game-repo &&
   pkg remove -y science-repo &&
   pkg update -y &&
   pkg -y upgr &&
   pkg i -y openssl-1.1 &&
   pgk i -y nodejs-lts &&
+  rm ~/../usr/etc/apt/apt.conf.d/local &&
   npm install homebridge &&
   npm install homebridge-config-ui-x

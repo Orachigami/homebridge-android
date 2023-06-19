b525d26fe81c6eed0c7c7722f43867c43a740811
Alexander <orachigami@mail.ru>
2023-06-19 03:31:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ export DEBIAN_FRONTEND=noninteractive &&
   rm ~/../usr/etc/apt/apt.conf.d/local &&
   npm install homebridge &&
   npm install -g --unsafe-perm homebridge-config-ui-x &&
-  echo -e '{\n    "bridge": {\n        "name": "Homebridge BA3D",\n        "username": "0E:F1:D3:85:BA:3D",\n        "port": 51248,\n        "pin": "171-94-744",\n        "advertiser": "bonjour-hap",\n        "log": {\n            "method": "file",\n            "path": "~/.homebridge/homebridge.log\n        }\n    },\n    "accessories": [],\n    "platforms": [\n        {\n            "name": "Config",\n            "port": 8581,\n            "platform": "config"\n        }\n    ]\n}\n' > ~/.homebridge/config.json &&
+  echo -e '{\n    "bridge": {\n        "name": "Homebridge BA3D",\n        "username": "0E:F1:D3:85:BA:3D",\n        "port": 51248,\n        "pin": "171-94-744",\n        "advertiser": "bonjour-hap",\n        "log": {\n            "method": "file",\n            "path": "~/.homebridge/homebridge.log"\n        }\n    },\n    "accessories": [],\n    "platforms": [\n        {\n            "name": "Config",\n            "port": 8581,\n            "platform": "config"\n        }\n    ]\n}\n' > ~/.homebridge/config.json &&
   echo -e '== SUCCESS ==\nTo run execute npx homebridge'
